4777d45faaa658d9ad50afe65a1155958aa84d4c
Avipatil2000 <avishkarpatil121@gmail.com>
2023-03-03 09:32:34 +0530

    thired Commite

--- ProjectMiddleTier/src/main/java/com/example/demo/controllers/ServiceProviderController.java
+++ ProjectMiddleTier/src/main/java/com/example/demo/controllers/ServiceProviderController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +39,10 @@ public class ServiceProviderController {
 		ServiceProvider sp=new ServiceProvider(ds.getName(),l);
 		return spservice.registerServiceProvider(sp);
 		
+	}
+	@GetMapping("/serviceproviders")
+	public List<ServiceProvider> getAll()
+	{
+		return spservice.getAll();
 	}
 }
--- ProjectMiddleTier/src/main/java/com/example/demo/services/ServiceProviderServices.java
+++ ProjectMiddleTier/src/main/java/com/example/demo/services/ServiceProviderServices.java
@@ -1,5 +1,7 @@
 package com.example.demo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,5 +22,10 @@ public class ServiceProviderServices {
 	{ 
 		return sprepo.save(sp);
 		
+	}
+	
+	public List<ServiceProvider> getAll()
+	{
+		return sprepo.findAll();
 	}
 }
